b86a1ebcdbcd945ce70be036c6560d9e7835b367
Eric Hsiao <ehsiao789@gmail.com>
2023-05-30 22:35:13 -0700

    finished ms4 part 1 trigger i think

--- src/main/webapp/static/ms4_triggers.sql
+++ src/main/webapp/static/ms4_triggers.sql
@@ -2,4 +2,34 @@
 
 -- PART 1:  Section 'LE', 'DI', and 'LAB' separation
 
-CREATE TRIGGER OVERLAP 
+CREATE OR REPLACE FUNCTION check_for_overlapping_meetings()
+RETURNS TRIGGER AS $$
+DECLARE
+    new_daysOfWeek text[];
+    existing_daysOfWeek text[];
+BEGIN
+    -- Convert the daysofweek strings to arrays for comparison
+    new_daysOfWeek := string_to_array(NEW.daysofweek, ',');
+    
+    -- Check for overlapping meetings with the same section (s_sectionID)
+    IF EXISTS (
+        SELECT 1
+        FROM weeklymeetings
+        WHERE s_sectionid = NEW.s_sectionid
+          AND (string_to_array(daysofweek, ',') && new_daysOfWeek)
+          AND ((starttime :: time, endtime :: time) OVERLAPS (NEW.starttime :: time, NEW.endtime :: time))
+    ) THEN
+        RAISE EXCEPTION 'Error: Booked Section % For the Same Time', NEW.s_sectionid;
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_check_for_overlapping_meetings
+BEFORE INSERT OR UPDATE ON weeklymeetings
+FOR EACH ROW
+EXECUTE FUNCTION check_for_overlapping_meetings();
+
+
+
