ed599833106510e6c32af6f7694905f83c652d26
Ji Hoon John Kim <chunikjh@gmail.com>
2023-06-05 10:38:24 -0700

    ms4_2 update still muted

--- src/main/webapp/static/ms4_2_triggers.sql
+++ src/main/webapp/static/ms4_2_triggers.sql
@@ -2,28 +2,32 @@
 
 -- PART 2: the enrollment limit then additional should be rejected.
 
--- CREATE OR REPLACE FUNCTION limit_instances()
--- RETURNS TRIGGER AS $$
--- DECLARE
---     count Capacity;
--- BEGIN
---     -- Check how many instances of the section 
---     SELECT COUNT(*) INTO Capacity FROM take 
---     WHERE cr_courseNumber = ? AND cl_title = ? AND cl_year = ? AND cl_quarter = ? AND s_sectionID = ? AND st_id = ?;
+CREATE OR REPLACE FUNCTION limit_instances()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Check how many instances of the section 
+    Capacity := SELECT COUNT(*) FROM take 
+    WHERE cr_courseNumber = ? AND cl_title = ? AND cl_year = ? AND cl_quarter = ? AND s_sectionID = ? AND st_id = ?;
     
---     -- if the count is greater than the limit, then raise an exception
---     IF Capacity > 50 THEN
---         RAISE EXCEPTION 'Cannot update. Maximum capacity exceeded';
---     END IF;
+    -- if the count is greater than the limit, then raise an exception
+    IF ( SELECT COUNT(*) FROM take 
+         WHERE cr_courseNumber :: VARCHAR(255) = NEW.cr_courseNumber :: VARCHAR(255)
+           AND cl_title :: VARCHAR(255) = NEW.cl_title :: VARCHAR(255)
+           AND cl_year :: INT = NEW.cl_year :: INT
+           AND cl_quarter :: VARCHAR(255) = NEW.cl_quarter :: VARCHAR(255)
+           AND s_sectionID :: VARCHAR(255) = NEW.s_sectionID :: VARCHAR(255)
+        ) > 50 THEN
+        RAISE EXCEPTION 'Error: Cannot update. Maximum capacity exceeded';
+    END IF;
   
---     RETURN NEW;
--- END;
--- $$ LANGUAGE plpgsql;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
--- CREATE TRIGGER trigger_limit_instances
--- BEFORE INSERT OR UPDATE ON take
--- FOR EACH ROW
--- EXECUTE FUNCTION limit_instances();
+CREATE TRIGGER trigger_limit_instances
+BEFORE INSERT OR UPDATE ON take
+FOR EACH ROW
+EXECUTE FUNCTION limit_instances();
 
 
 
